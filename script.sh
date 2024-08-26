58fd546d35737f4d2483e8672dd317ec429ae93e
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-08-25 22:20:40 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Masood-J/local_manifests.git -b SigmaDroid-14 .repo/local_manifests
+git clone https://github.com/MA3OOD/local_manifests.git -b SigmaDroid-14 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -19,16 +19,6 @@ echo "============================"
 echo "============="
 echo "Sync success"
 echo "============="
-
-# Remove overrides
-# Define a list of packages to remove
-echo "===== Remove overrides started ====="
-
-OVER_PACKAGES=("GoogleContacts" "GoogleDialer" "PrebuiltBugle" "dialer")
-for PACKAGEU in "${OVER_PACKAGES[@]}"; do
-find vendor/gms -name 'common-vendor.mk' -exec sed -i "/$PACKAGEU/d" {} \;
-done
-echo "===== Remove overrides Success ====="
 
 # Export
 export BUILD_USERNAME=Masood•BecomingTooSigma
