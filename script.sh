98935446a87963363ad91dd0d107bcea6d30bb2d
Muhammad Masood <163732055+Masood-J@users.noreply.github.com>
2024-09-01 00:10:45 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -28,7 +28,7 @@ export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 echo "====== Envsetup Done ======="
 
 # Lunch
-lunch lineage_a10-ap2a-user
+lunch lineage_a10-ap2a-user || lunch lineage_a10-user
 make installclean
 echo "============="
 # Build ROM
