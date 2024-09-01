a15439dcb192c8e59a430b980345a92d01f7b51e
Muhammad Masood <163732055+Masood-J@users.noreply.github.com>
2024-09-01 05:03:08 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -22,11 +22,12 @@ echo "============="
 
 # Export
 echo "======= Export Done ======"
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+
 # Set up build environment
 export WITH_GMS=true
 export TARGET_USES_MINI_GAPPS=true
 export TARGET_USES_PICO_GAPPS=false
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
