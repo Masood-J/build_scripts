a8012ba8c873f3e14b8f85b64e6aa4fb92d094cb
Muhammad Masood <163732055+Masood-J@users.noreply.github.com>
2024-09-13 11:10:36 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -92,7 +92,7 @@ if [ -f "device/samsung/a10/AndroidProducts.mk" ]; then
 
     cat > device/samsung/a10/AndroidProducts.mk << 'EOF'
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a10.mk
+    device/samsung/a10/lineage_a10.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_a10-user \
