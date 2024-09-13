a552b2ac60e7d74d162061d821345b0989e6a727
Muhammad Masood <163732055+Masood-J@users.noreply.github.com>
2024-09-13 11:07:10 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -72,6 +72,8 @@ $(call inherit-product, vendor/gms/gms_full.mk)
 endif
 endif
 
+BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a10
 PRODUCT_NAME := lineage_a10
