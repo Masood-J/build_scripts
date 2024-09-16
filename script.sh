16a90beeb60eb31d8b16fd94d1f59f8426be4200
Muhammad Masood <163732055+Masood-J@users.noreply.github.com>
2024-09-16 16:42:07 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -32,19 +32,19 @@ export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 echo "====== Envsetup Done ======="
 
 # Rename pixel-style_a10.mk to lineage_a10.mk after envsetup
-if [ -f "device/samsung/a30/pixel-style_a30.mk" ]; then
-    echo "Renaming pixel-style_a30.mk to lineage_a30.mk..."
-    mv device/samsung/a30/pixel-style_a30.mk device/samsung/a30/lineage_a30.mk
+if [ -f "device/samsung/a30s/pixel-style_a30s.mk" ]; then
+    echo "Renaming pixel-style_a30s.mk to lineage_a30s.mk..."
+    mv device/samsung/a30s/pixel-style_a30s.mk device/samsung/a30s/lineage_a30s.mk
     echo "Rename successful."
 else
-    echo "pixel-style_a30.mk not found. Skipping rename."
+    echo "pixel-style_a30s.mk not found. Skipping rename."
 fi
 
 # Modify lineage_a30.mk
-if [ -f "device/samsung/a30/lineage_a30.mk" ]; then
-    echo "Modifying lineage_a30.mk..."
+if [ -f "device/samsung/a30s/lineage_a30s.mk" ]; then
+    echo "Modifying lineage_a30s.mk..."
 
-    cat > device/samsung/a30/lineage_a30.mk << 'EOF'
+    cat > device/samsung/a30s/lineage_a30s.mk << 'EOF'
 # Copyright (C) 2018 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 
@@ -55,7 +55,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/a30/device.mk)
+$(call inherit-product, device/samsung/a30s/device.mk)
 
 # Inherit from common lineage configuration
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
@@ -86,46 +86,46 @@ endif
 
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := a30
-PRODUCT_NAME := lineage_a30
-PRODUCT_MODEL := SM-A305F
+PRODUCT_DEVICE := a30s
+PRODUCT_NAME := lineage_a30s
+PRODUCT_MODEL := SM-A307F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 EOF
 
-    echo "lineage_a30.mk modified successfully."
+    echo "lineage_a30s.mk modified successfully."
 fi
 
 # Modify AndroidProducts.mk
-if [ -f "device/samsung/a30/AndroidProducts.mk" ]; then
+if [ -f "device/samsung/a30s/AndroidProducts.mk" ]; then
     echo "Modifying AndroidProducts.mk..."
 
-    cat > device/samsung/a30/AndroidProducts.mk << 'EOF'
+    cat > device/samsung/a30s/AndroidProducts.mk << 'EOF'
 PRODUCT_MAKEFILES := \
-    device/samsung/a30/lineage_a30.mk
+    device/samsung/a30s/lineage_a30s.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_a30-user \
-    lineage_a30-userdebug
+    lineage_a30s-user \
+    lineage_a30s-userdebug
 EOF
 
     echo "AndroidProducts.mk modified successfully."
 fi
 
-if [ -f "device/samsung/a20/pixel-style_a20.mk" ]; then
-    echo "Renaming pixel-style_a20.mk to lineage_a20.mk..."
-    mv device/samsung/a20/pixel-style_a20.mk device/samsung/a20/lineage_a20.mk
+if [ -f "device/samsung/a40/pixel-style_a40.mk" ]; then
+    echo "Renaming pixel-style_a40.mk to lineage_a40.mk..."
+    mv device/samsung/a40/pixel-style_a40.mk device/samsung/a40/lineage_a40.mk
     echo "Rename successful."
 else
-    echo "pixel-style_a20.mk not found. Skipping rename."
+    echo "pixel-style_a40.mk not found. Skipping rename."
 fi
 
-# Modify lineage_a10.mk
-if [ -f "device/samsung/a20/lineage_a20.mk" ]; then
-    echo "Modifying lineage_a20.mk..."
+# Modify lineage_a40.mk
+if [ -f "device/samsung/a40/lineage_a40.mk" ]; then
+    echo "Modifying lineage_a40.mk..."
 
-    cat > device/samsung/a20/lineage_a20.mk << 'EOF'
+    cat > device/samsung/a40/lineage_a40.mk << 'EOF'
 # Copyright (C) 2018 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 
@@ -136,7 +136,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/a20/device.mk)
+$(call inherit-product, device/samsung/a40/device.mk)
 
 # Inherit from common lineage configuration
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
@@ -167,109 +167,28 @@ endif
 
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := a20
-PRODUCT_NAME := lineage_a20
-PRODUCT_MODEL := SM-A205F
+PRODUCT_DEVICE := a40
+PRODUCT_NAME := lineage_a40
+PRODUCT_MODEL := SM-A405F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 EOF
 
-    echo "lineage_a20.mk modified successfully."
+    echo "lineage_a40.mk modified successfully."
 fi
 
 # Modify AndroidProducts.mk
-if [ -f "device/samsung/a20/AndroidProducts.mk" ]; then
+if [ -f "device/samsung/a40/AndroidProducts.mk" ]; then
     echo "Modifying AndroidProducts.mk..."
 
-    cat > device/samsung/a20/AndroidProducts.mk << 'EOF'
+    cat > device/samsung/a40/AndroidProducts.mk << 'EOF'
 PRODUCT_MAKEFILES := \
-    device/samsung/a20/lineage_a20.mk
+    device/samsung/a40/lineage_a40.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_a20-user \
-    lineage_a20-userdebug
-EOF
-
-    echo "AndroidProducts.mk modified successfully."
-fi
-
-if [ -f "device/samsung/a20e/pixel-style_a20e.mk" ]; then
-    echo "Renaming pixel-style_a20e.mk to lineage_a20e.mk..."
-    mv device/samsung/a20e/pixel-style_a20e.mk device/samsung/a20e/lineage_a20e.mk
-    echo "Rename successful."
-else
-    echo "pixel-style_a20e.mk not found. Skipping rename."
-fi
-
-# Modify lineage_a10.mk
-if [ -f "device/samsung/a20e/lineage_a20e.mk" ]; then
-    echo "Modifying lineage_a20e.mk..."
-
-    cat > device/samsung/a20e/lineage_a20e.mk << 'EOF'
-# Copyright (C) 2018 The LineageOS Project
-# SPDX-License-Identifier: Apache-2.0
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/samsung/a20e/device.mk)
-
-# Inherit from common lineage configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# ROM Flags
-BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
-EVO_BUILD_TYPE := Unofficial
-TARGET_DISABLE_EPPE := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_BUILD_APERTURE_CAMERA := false
-TARGET_HAS_UDFPS := true
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1520
-TARGET_SCREEN_WIDTH := 720
-
-# GMS
-WITH_GMS ?= false
-ifeq ($(WITH_GMS),true)
-ifeq ($(TARGET_USES_MINI_GAPPS),true)
-$(call inherit-product, vendor/gms/gms_mini.mk)
-else ifeq ($(TARGET_USES_PICO_GAPPS),true)
-$(call inherit-product, vendor/gms/gms_pico.mk)
-else
-$(call inherit-product, vendor/gms/gms_full.mk)
-endif
-endif
-
-
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := a20e
-PRODUCT_NAME := lineage_a20e
-PRODUCT_MODEL := SM-A202K
-PRODUCT_BRAND := samsung
-PRODUCT_MANUFACTURER := samsung
-PRODUCT_GMS_CLIENTID_BASE := android-samsung
-EOF
-
-    echo "lineage_a20e.mk modified successfully."
-fi
-
-# Modify AndroidProducts.mk
-if [ -f "device/samsung/a20e/AndroidProducts.mk" ]; then
-    echo "Modifying AndroidProducts.mk..."
-
-    cat > device/samsung/a20e/AndroidProducts.mk << 'EOF'
-PRODUCT_MAKEFILES := \
-    device/samsung/a20e/lineage_a20e.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_a20e-user \
-    lineage_a20e-userdebug
+    lineage_a40-user \
+    lineage_a40-userdebug
 EOF
 
     echo "AndroidProducts.mk modified successfully."
@@ -277,6 +196,6 @@ fi
 
 
 # Attempt first lunch target
-lunch lineage_a30-user && make installclean && m evolution && lunch lineage_a20-user && make installclean && m evolution && lunch lineage_a20e-user && make installclean && m evolution
+lunch lineage_a30s-user && make installclean && m evolution && lunch lineage_a40-user && make installclean && m evolution 
 
 echo "Build process completed."
