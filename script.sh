aa58cb34ed1ee54935a07ab95e0ab2c3da5a00f4
Muhammad Masood <163732055+Masood-J@users.noreply.github.com>
2024-09-18 17:52:10 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@
 rm -rf .repo/local_manifests/
 
 # Initialize the repo
-repo init -u https://github.com/Miku-UI/manifesto -b Udon_v2
+repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
@@ -24,33 +24,12 @@ echo "============="
 # Export environment variables
 export BUILD_USERNAME=Masood
 export BUILD_HOSTNAME=crave
-export MIKU_MASTER=Masood
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export MIKU_GAPPS=false
-export TARGET_WITH_KERNEL_SU=true
-export TARGET_RELEASE=ap2a
 echo "======= Export Done ======"
 
 # Set up the build environment
 . build/envsetup.sh
 echo "====== Envsetup Done ======="
-# Modify release.mk to update TARGET_RELEASE if needed
-RELEASE_MK_PATH="build/make/core/release_config.mk"
-
-# Backup the original file before making changes
-cp "$RELEASE_MK_PATH" "$RELEASE_MK_PATH.bak"
-
-# Check if the file contains the default TARGET_RELEASE setting
-if grep -q 'TARGET_RELEASE = trunk_staging' "$RELEASE_MK_PATH"; then
-    echo "Updating TARGET_RELEASE default value to 'ap2a'"
-
-    # Use sed to replace 'trunk_staging' with 'ap2a'
-    sed -i 's/TARGET_RELEASE = trunk_staging/TARGET_RELEASE = ap2a/' "$RELEASE_MK_PATH"
-else
-    echo "TARGET_RELEASE default value is already set to something other than 'trunk_staging' or not found."
-fi
-
 # Build for A10
-lunch miku_a10-ap2a-user
+lunch lineage_a10-ap2a-user
 make installclean
-make diva
+mka bacon
