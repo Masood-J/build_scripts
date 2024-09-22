4622683f434a8a4750269aff9f14ac05fe01d43e
Muhammad Masood <163732055+Masood-J@users.noreply.github.com>
2024-09-22 15:37:13 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -149,7 +149,62 @@ COMMON_LUNCH_CHOICES := \
 EOF
 fi
 
+cat > device/samsung/a30/lineage_a30.mk << 'EOF'
+# Copyright (C) 2018 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/a30/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+
+# Rom Specific Flags
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := false
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BUILD_PACKAGE := 2
+TARGET_LAUNCHER := 1
+
+# Maintainer
+ALPHA_BUILD_TYPE := Official
+ALPHA_MAINTAINER := Masood
+
+# Device identifier
+PRODUCT_DEVICE := a30
+PRODUCT_NAME := lineage_a30
+PRODUCT_MODEL := SM-A305F
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+EOF
+
+# Modify AndroidProducts.mk for A10
+if [ -f "device/samsung/a30/AndroidProducts.mk" ]; then
+    echo "Modifying AndroidProducts.mk for A30..."
+    
+    # Overwrite AndroidProducts.mk with the desired contents
+    cat > device/samsung/a30/AndroidProducts.mk << 'EOF'
+PRODUCT_MAKEFILES := \
+    device/samsung/a30/lineage_a30.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_a30-eng \
+    lineage_a30-user \
+    lineage_a30-userdebug
+EOF
+fi
+
 # Step 4: Continue with the build process
 
 # Build for A10
-lunch lineage_a20-user && make installclean && make bacon && lunch lineage_a20e-user && make installclean && make bacon
+lunch lineage_a20-user && make installclean && make bacon && lunch lineage_a20e-user && make installclean && make bacon && lunch lineage_a30-user && make installclean && make bacon
