44c42657c49d5dc51b426789531e24e97744e410
Muhammad Masood <163732055+Masood-J@users.noreply.github.com>
2024-09-22 20:12:33 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+repo init -u https://github.com/StatiXOS/android_manifest.git -b udc-qpr3
 echo "=================="
 echo "Repo init success"
 echo "=================="
