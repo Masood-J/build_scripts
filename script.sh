6660397b7bba076baf15aa7a4d8ec09ffdfe3c7d
Muhammad Masood <163732055+Masood-J@users.noreply.github.com>
2024-09-24 00:21:30 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -47,7 +47,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configuration
-$(call inherit-product, device/samsung/a10/device.mk)
+$(call inherit-product, device/samsung/device.mk)
 
 # Inherit some common GenesisOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
