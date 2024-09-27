ad2767cf23b61aef91ae2d1654d7a31a0edb154e
Muhammad Masood <163732055+Masood-J@users.noreply.github.com>
2024-09-27 21:50:00 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -28,7 +28,7 @@ echo "======= Export Done ======"
 # Set up build environment
 . build/envsetup.sh
 echo "====== Envsetup Done ======="
-cat > device/samsung/a20/genesis_a20.mk << 'EOF'
+cat > device/samsung/a30/genesis_a30.mk << 'EOF'
 # Copyright (C) 2018 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 # Inherit from those products. Most specific first.
@@ -37,30 +37,30 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configuration
-$(call inherit-product, device/samsung/a20/device.mk)
+$(call inherit-product, device/samsung/a30/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/genesis/config/common_full_phone.mk)
 # Set the maintainer property for the ROM
 PRODUCT_PROPERTY_OVERRIDES := ro.genesis.maintainer=Masood
 # Device identifier
-PRODUCT_DEVICE := a20
-PRODUCT_NAME := genesis_a20
-PRODUCT_MODEL := SM-A205F
+PRODUCT_DEVICE := a30
+PRODUCT_NAME := genesis_a30
+PRODUCT_MODEL := SM-A305F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 EOF
 # Modify AndroidProducts.mk for A10
-cat > device/samsung/a20/AndroidProducts.mk << 'EOF'
+cat > device/samsung/a30/AndroidProducts.mk << 'EOF'
 PRODUCT_MAKEFILES := \
-    device/samsung/a20/genesis_a20.mk
+    device/samsung/a30/genesis_a30.mk
 COMMON_LUNCH_CHOICES := \
-    genesis_a20-eng \
-    genesis_a20-user \
-    genesis_a20-userdebug
+    genesis_a30-eng \
+    genesis_a30-user \
+    genesis_a30-userdebug
 EOF
 
-cat > device/samsung/a20e/genesis_a20e.mk << 'EOF'
+cat > device/samsung/a30s/genesis_a30s.mk << 'EOF'
 # Copyright (C) 2018 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 # Inherit from those products. Most specific first.
@@ -69,28 +69,28 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configuration
-$(call inherit-product, device/samsung/a20e/device.mk)
+$(call inherit-product, device/samsung/a30s/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/genesis/config/common_full_phone.mk)
 # Set the maintainer property for the ROM
 PRODUCT_PROPERTY_OVERRIDES := ro.genesis.maintainer=Masood
 # Device identifier
-PRODUCT_DEVICE := a20e
-PRODUCT_NAME := genesis_a20e
-PRODUCT_MODEL := SM-A202K
+PRODUCT_DEVICE := a30s
+PRODUCT_NAME := genesis_a30s
+PRODUCT_MODEL := SM-A307F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 EOF
 # Modify AndroidProducts.mk for A10
-cat > device/samsung/a20e/AndroidProducts.mk << 'EOF'
+cat > device/samsung/a30s/AndroidProducts.mk << 'EOF'
 PRODUCT_MAKEFILES := \
-    device/samsung/a20e/genesis_a20e.mk
+    device/samsung/a30s/genesis_a30s.mk
 COMMON_LUNCH_CHOICES := \
-    genesis_a20e-eng \
-    genesis_a20e-user \
-    genesis_a20e-userdebug
+    genesis_a30s-eng \
+    genesis_a30s-user \
+    genesis_a30s-userdebug
 EOF
 
 # Build for A10
-lunch genesis_a20-user && make installclean && mka genesis && lunch genesis_a20e-user && make installclean && mka genesis
+lunch genesis_a30-user && make installclean && mka genesis && lunch genesis_a30s-user && make installclean && mka genesis
