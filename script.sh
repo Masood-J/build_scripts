ee671902d471912018e7efb057a9bba96fb245fd
Muhammad Masood <163732055+Masood-J@users.noreply.github.com>
2024-10-04 18:27:55 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -34,21 +34,21 @@ echo "====== Envsetup Done ======"
 mkdir -p vendor/aospa/products/a10
 
 # Write the aospa_a10.mk file
-cat <<EOF > vendor/aospa/products/a10/aospa_a10.mk
+cat > vendor/aospa/products/a10/aospa_a10.mk << 'EOF'
 # Copyright (C) 2018 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 
 # Inherit from those products. Most specific first.
-\$(call inherit-product, \$(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
-\$(call inherit-product, \$(SRC_TARGET_DIR)/product/core_64_bit.mk)
-\$(call inherit-product, \$(SRC_TARGET_DIR)/product/full_base.mk)
-\$(call inherit-product, \$(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configuration
-\$(call inherit-product, device/samsung/a10/device.mk)
+$(call inherit-product, device/samsung/a10/device.mk)
 
 # Inherit from the AOSPA configuration.
-\$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
 
 # Device identifier
 PRODUCT_DEVICE := a10
