b9ae9ec8dce0b2b6ba4065ac287666ea464adfdf
Muhammad Masood <163732055+Masood-J@users.noreply.github.com>
2024-10-08 07:53:24 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@
 rm -rf .repo/local_manifests/
 
 # Initialize the repo for AOSPA (Uvite branch)
-repo init -u https://github.com/AOSPA/manifest -b uvite
+repo init -u https://github.com/TenX-OS/manifest.git -b fourteen --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
@@ -31,10 +31,9 @@ echo "======= Export Done ======"
 echo "====== Envsetup Done ======"
 
 # Create the missing 'a10' directory if it doesn't exist
-mkdir -p vendor/aospa/products/a10
 # Write the aospa_a10.mk file
 
-cat > vendor/aospa/products/a10/aospa_a10.mk << 'EOF'
+cat > device/samsung/a10/lineage_a10.mk << 'EOF'
 # Copyright (C) 2018 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 
@@ -47,29 +46,35 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/a10/device.mk)
 
-# Inherit from the AOSPA configuration.
-$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# TenX
+TARGET_HAS_UDFPS := false
+TARGET_ENABLE_BLUR := false
+WITH_GMS := false
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
 PRODUCT_DEVICE := a10
-PRODUCT_NAME := aospa_a10
+PRODUCT_NAME := lineage_a10
 PRODUCT_MODEL := SM-A105F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 EOF
 
-cat > vendor/aospa/products/AndroidProducts.mk << 'EOF'
+cat > device/samsung/a10/AndroidProducts.mk << 'EOF'
 PRODUCT_MAKEFILES := \
-    vendor/aospa/products/a10/aospa_a10.mk
+    device/samsung/a10/lineage_a10.mk
 COMMON_LUNCH_CHOICES := \
-    aospa_a10-eng \
-    aospa_a10-user \
-    aospa_a10-userdebug
+    lineage_a10-eng \
+    lineage_a10-user \
+    lineage_a10-userdebug
 EOF
 
 # Write the aospa.dependencies file
 echo "====== aospa_a10.mk Created ======"
 
 # Build for A10
-lunch aospa_a10-user && make installclean && m bacon || ./rom-build.sh a10
+lunch lineage_a10-ap2a-userdebug && make installclean && mka bacon
