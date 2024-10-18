95758f638d9853b14593014887801f1cbd64dda1
Muhammad Masood <163732055+Masood-J@users.noreply.github.com>
2024-10-18 15:59:10 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -29,14 +29,14 @@ echo "======= Export Done ======"
 # Set up build environment
 . build/envsetup.sh
 echo "====== Envsetup Done ======="
-if [ -f "device/samsung/a10/pixel-style_a10.mk" ]; then
-    echo "Renaming pixel-style_a10.mk to lineage_a10.mk..."
-    mv device/samsung/a10/pixel-style_a10.mk device/samsung/a10/lineage_a10.mk
+if [ -f "device/samsung/a30/pixel-style_a30.mk" ]; then
+    echo "Renaming pixel-style_a30.mk to lineage_a30.mk..."
+    mv device/samsung/a30/pixel-style_a30.mk device/samsung/a30/lineage_a30.mk
     echo "Rename successful."
 else
     echo "pixel-style_a10.mk not found. Skipping rename."
 fi
-cat > device/samsung/a10/lineage_a10.mk << 'EOF'
+cat > device/samsung/a30/lineage_a30.mk << 'EOF'
 # Copyright (C) 2018 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 # Inherit from those products. Most specific first.
@@ -45,7 +45,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configuration
-$(call inherit-product, device/samsung/a10/device.mk)
+$(call inherit-product, device/samsung/a30/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
@@ -63,24 +63,24 @@ TARGET_SCREEN_WIDTH := 720
 TARGET_USES_PICO_GAPPS := true
 
 # Device identifier
-PRODUCT_DEVICE := a10
-PRODUCT_NAME := lineage_a10
-PRODUCT_MODEL := SM-A105F
+PRODUCT_DEVICE := a30
+PRODUCT_NAME := lineage_a30
+PRODUCT_MODEL := SM-A305F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 EOF
 # Modify AndroidProducts.mk for A10
-cat > device/samsung/a10/AndroidProducts.mk << 'EOF'
+cat > device/samsung/a30/AndroidProducts.mk << 'EOF'
 PRODUCT_MAKEFILES := \
-    device/samsung/a10/lineage_a10.mk
+    device/samsung/a30/lineage_a10.mk
 COMMON_LUNCH_CHOICES := \
-    lineage_a10-eng \
-    lineage_a10-user \
-    lineage_a10-userdebug
+    lineage_a30-eng \
+    lineage_a30-user \
+    lineage_a30-userdebug
 EOF
 
 # Build for A10
-lunch lineage_a10-user
+lunch lineage_a30-user
 make installclean 
 m evolution  
