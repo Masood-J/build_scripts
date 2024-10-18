4f8704492de2acd4745b6b4ed3b31d244f7755e5
Muhammad Masood <163732055+Masood-J@users.noreply.github.com>
2024-10-18 21:24:12 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -29,16 +29,16 @@ echo "======= Export Done ======"
 # Set up build environment
 . build/envsetup.sh
 echo "====== Envsetup Done ======="
-if [ -f "device/samsung/a30/pixel-style_a30.mk" ]; then
+if [ -f "device/samsung/a30s/pixel-style_a30s.mk" ]; then
     echo "Renaming pixel-style_a30.mk to lineage_a30.mk..."
-    mv device/samsung/a30/pixel-style_a30.mk device/samsung/a30/lineage_a30.mk
-    mv device/samsung/a20/pixel-style_a20.mk device/samsung/a20/lineage_a20.mk
+    mv device/samsung/a40/pixel-style_a30.mk device/samsung/a30/lineage_a30.mk
+    mv device/samsung/a20e/pixel-style_a20.mk device/samsung/a20/lineage_a20.mk
     mv device/samsung/a30s/pixel-style_a30s.mk device/samsung/a30s/lineage_a30s.mk
     echo "Rename successful."
 else
     echo "pixel-style_a10.mk not found. Skipping rename."
 fi
-cat > device/samsung/a30/lineage_a30.mk << 'EOF'
+cat > device/samsung/a40/lineage_a40.mk << 'EOF'
 # Copyright (C) 2018 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 # Inherit from those products. Most specific first.
@@ -47,7 +47,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configuration
-$(call inherit-product, device/samsung/a30/device.mk)
+$(call inherit-product, device/samsung/a40/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
@@ -65,24 +65,24 @@ TARGET_SCREEN_WIDTH := 720
 TARGET_USES_PICO_GAPPS := true
 
 # Device identifier
-PRODUCT_DEVICE := a30
-PRODUCT_NAME := lineage_a30
-PRODUCT_MODEL := SM-A305F
+PRODUCT_DEVICE := a40
+PRODUCT_NAME := lineage_a40
+PRODUCT_MODEL := SM-A405F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 EOF
 # Modify AndroidProducts.mk for A10
-cat > device/samsung/a30/AndroidProducts.mk << 'EOF'
+cat > device/samsung/a40/AndroidProducts.mk << 'EOF'
 PRODUCT_MAKEFILES := \
-    device/samsung/a30/lineage_a30.mk
+    device/samsung/a40/lineage_a40.mk
 COMMON_LUNCH_CHOICES := \
-    lineage_a30-eng \
-    lineage_a30-user \
-    lineage_a30-userdebug
+    lineage_a40-eng \
+    lineage_a40-user \
+    lineage_a40-userdebug
 EOF
 
-cat > device/samsung/a20/lineage_a20.mk << 'EOF'
+cat > device/samsung/a20e/lineage_a20e.mk << 'EOF'
 # Copyright (C) 2018 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 # Inherit from those products. Most specific first.
@@ -91,7 +91,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configuration
-$(call inherit-product, device/samsung/a20/device.mk)
+$(call inherit-product, device/samsung/a20e/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
@@ -109,21 +109,21 @@ TARGET_SCREEN_WIDTH := 720
 TARGET_USES_PICO_GAPPS := true
 
 # Device identifier
-PRODUCT_DEVICE := a20
-PRODUCT_NAME := lineage_a20
-PRODUCT_MODEL := SM-A205F
+PRODUCT_DEVICE := a20e
+PRODUCT_NAME := lineage_a20e
+PRODUCT_MODEL := SM-A207K
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 EOF
 # Modify AndroidProducts.mk for A10
-cat > device/samsung/a20/AndroidProducts.mk << 'EOF'
+cat > device/samsung/a20e/AndroidProducts.mk << 'EOF'
 PRODUCT_MAKEFILES := \
-    device/samsung/a20/lineage_a20.mk
+    device/samsung/a20e/lineage_a20e.mk
 COMMON_LUNCH_CHOICES := \
-    lineage_a20-eng \
-    lineage_a20-user \
-    lineage_a20-userdebug
+    lineage_a20e-eng \
+    lineage_a20e-user \
+    lineage_a20e-userdebug
 EOF
 
 cat > device/samsung/a30s/lineage_a30s.mk << 'EOF'
@@ -171,5 +171,5 @@ COMMON_LUNCH_CHOICES := \
 EOF
 
 # Build for A10
-lunch lineage_a30-user && make installclean && m evolution && lunch lineage_a20-user && make installclean && m evolution && lunch lineage_a30s-user && make installclean && m evolution
+lunch lineage_a20e-user && make installclean && m evolution && lunch lineage_a40-user && make installclean && m evolution && lunch lineage_a30s-user && make installclean && m evolution
 
